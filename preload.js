1dffe07adc59e3adcaf6c2ae55cbd701b32febd9
SauravPahuja <58905716+SauravPahuja1212@users.noreply.github.com>
2024-04-19 01:46:45 -0400

    Added ipc module for inter process communication

--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
@@ -10,4 +12,15 @@ window.addEventListener("DOMContentLoaded", () => {
   for (const dependency of ["chrome", "node", "electron"]) {
     replaceText(`${dependency}-version`, process.versions[dependency]);
   }
+});
+
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+
+  //Custom function to do something in main process from renderer process,
+  //we never expose full ipcRenderer module via preload as this can be a powerful attack vector for malicious code
+  ping: () => ipcRenderer.invoke("ping"),
+  //we can also expose cariables, not just functions
 });
